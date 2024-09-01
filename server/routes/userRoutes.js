626ca2ed56b9483c34d9c54fe82c23cfd5511b0a
Alan Mong <alanmongx@gmail.com>
2024-09-01 02:27:33 -0700

    add landing page

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,14 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser, getUserProfile } = require('../controllers/userController');
-const { protect } = require('../middlewares/authMiddleware');
+const {
+  registerUser,
+  loginUser,
+  getUserProfile,
+} = require("../controllers/userController");
+const { protect } = require("../middlewares/authMiddleware");
 
+//api/users/test
 router.get("/test", (req, res) => {
   res.json({ message: "API is working properly!" });
 });
 
-router.post('/register', registerUser);
-router.post('/login', loginUser);
-router.get('/profile', protect, getUserProfile);
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/profile", protect, getUserProfile);
 
 module.exports = router;
