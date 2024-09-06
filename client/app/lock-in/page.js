a8dce6b4ca069f16d5aa28d481c786a20db7a91a
Alan Mong <alanmongx@gmail.com>
2024-09-05 17:56:09 -0700

    add lock-in component to track user time spent locked in in addition to allow pausing and quitting of timer

---
+++ client/app/lock-in/page.js
@@ -0,0 +1,10 @@
+// app/lock-in/page.js
+import LockInTimer from "./components/LockInTimer"; // Adjust the path based on your folder structure
+
+export default function LockInPage() {
+  return (
+    <div>
+      <LockInTimer />
+    </div>
+  );
+}
