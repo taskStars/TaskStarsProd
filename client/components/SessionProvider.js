0c16d332b7277cac7422b32322ae9f40d411a52c
Alan Mong <alanmongx@gmail.com>
2024-09-15 16:20:57 -0700

    refactored and removed next-auth

--- client/components/SessionProvider.js
+++
@@ -1,8 +0,0 @@
-// components/SessionProviderWrapper.js
-"use client";
-
-import { SessionProvider } from "next-auth/react";
-
-export default function SessionProviderWrapper({ children, session }) {
-  return <SessionProvider session={session}>{children}</SessionProvider>;
-}
