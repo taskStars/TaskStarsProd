ee2586b83ce8a27ccb44d98094a340bf5007ab15
Alan Mong <alanmongx@gmail.com>
2024-09-15 18:30:22 -0700

    ready to deploy

--- server/server.js
+++ server/server.js
@@ -1,5 +1,12 @@
-// Load environment variables
-require("dotenv").config();
+// Load environment variables using dotenv
+const dotenv = require("dotenv");
+
+// Dynamically load the appropriate .env file based on the environment
+if (process.env.NODE_ENV === "production") {
+  dotenv.config({ path: ".env.production" });
+} else {
+  dotenv.config({ path: ".env.local" });
+}
 
 // Import necessary packages
 const express = require("express");
@@ -12,6 +19,11 @@ const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const taskRoutes = require("./routes/taskRoutes");
 const productivityRoutes = require("./routes/productivityRoutes");
+
+// Initialize Next.js application
+const dev = process.env.NODE_ENV !== "production";
+const nextApp = next({ dev, dir: "./client" }); // Ensure this path is relative to where server.js is located
+const handle = nextApp.getRequestHandler();
 
 // Initialize Express app and server
 const app = express();
@@ -29,11 +41,6 @@ const io = new Server(server, {
 
 // Database Connection
 connectDB();
-
-// Next.js setup
-const dev = process.env.NODE_ENV !== "production";
-const nextApp = next({ dev, dir: "../client" });
-const handle = nextApp.getRequestHandler();
 
 // Middleware
 app.use(
@@ -59,13 +66,13 @@ app.get("/test", (req, res) => {
   res.json({ message: "API is working properly!" });
 });
 
-// Routes
+// API Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/tasks", taskRoutes(io)); // Pass io to task routes
 app.use("/api/productivity", productivityRoutes);
 
-// Next.js handling
+// Next.js request handling
 nextApp
   .prepare()
   .then(() => {
